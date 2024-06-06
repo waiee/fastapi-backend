ff3286f2d35032ac405680a2febd6c5ec03341e5
Waiee Zainol <69743222+waiee@users.noreply.github.com>
2024-06-07 04:20:52 +0800

    c

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return{"Testing": "123"}
+    return {"Testing": "123"}
+
+# Global list to store items
+items_list = []
 
 @app.post("/items")
-def create_item(items:str):
-    items.append(items)
-    return items
+def create_item(items: str):
+    items_list.append(items)
+    return items_list
