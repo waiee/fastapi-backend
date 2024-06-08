5f95d9ac3167ceb2c372aec60d304a05509ad781
Waiee Zainol <69743222+waiee@users.noreply.github.com>
2024-06-08 10:00:02 +0800

    c

--- app/schemas.py
+++ app/schemas.py
@@ -1,10 +1,16 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class UserBase(BaseModel):
     email: str
 
 class UserCreate(UserBase):
     password: str
+
+class UserUpdate(BaseModel):
+    email: Optional[str] = None
+    password: Optional[str] = None
+    is_active: Optional[bool] = None
 
 class User(UserBase):
     id: int
