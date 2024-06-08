2de0d2b3c51eadcd4e3654bde091402426b76a0e
Waiee Zainol <69743222+waiee@users.noreply.github.com>
2024-06-08 10:25:37 +0800

    c

--- test.py
+++ test.py
@@ -1,38 +1,38 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+# from fastapi import FastAPI, HTTPException
+# from pydantic import BaseModel
 
-app = FastAPI()
+# app = FastAPI()
 
-class Item(BaseModel):
-    text: str = None
-    is_done: bool = False
+# class Item(BaseModel):
+#     text: str = None
+#     is_done: bool = False
 
-@app.get("/")
-def root():
-    return {"Testing": "123"}
-
-# Global list to store items
-items_list = []
-@app.post("/items")
-def create_item(items: Item):
-    items_list.append(items)
-    return items_list
-
-@app.get("/items/{items_id}")
-def get_item(items_id: int) -> Item:
-    if items_id >= len(items_list) or items_id < 0:
-        raise HTTPException(status_code=404, detail="Item not found")
-    return items_list[items_id]
+# @app.get("/")
+# def root():
+#     return {"Testing": "123"}
 
 # # Global list to store items
 # items_list = []
 # @app.post("/items")
-# def create_item(items: str):
+# def create_item(items: Item):
 #     items_list.append(items)
 #     return items_list
 
 # @app.get("/items/{items_id}")
-# def get_item(items_id: int):
+# def get_item(items_id: int) -> Item:
 #     if items_id >= len(items_list) or items_id < 0:
 #         raise HTTPException(status_code=404, detail="Item not found")
 #     return items_list[items_id]
+
+# # # Global list to store items
+# # items_list = []
+# # @app.post("/items")
+# # def create_item(items: str):
+# #     items_list.append(items)
+# #     return items_list
+
+# # @app.get("/items/{items_id}")
+# # def get_item(items_id: int):
+# #     if items_id >= len(items_list) or items_id < 0:
+# #         raise HTTPException(status_code=404, detail="Item not found")
+# #     return items_list[items_id]
