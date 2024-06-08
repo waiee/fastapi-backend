1749d3d2c72684be7d35d3fada2dbae8e82bdd62
Waiee Zainol <69743222+waiee@users.noreply.github.com>
2024-06-08 20:02:34 +0800

    routers gradio

--- app/dependencies.py
+++ app/dependencies.py
@@ -5,3 +5,10 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ app/routers/__init__.py
@@ -0,0 +1,3 @@
+from . import user, company, gradio  # Import the gradio router here
+
+__all__ = ["user", "company", "gradio"]  # Optionally, define __all__ to export modules
