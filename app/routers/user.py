45ec41efb5ecf6a3607e72c03fb7c80ec2a799f5
Waiee Zainol <69743222+waiee@users.noreply.github.com>
2024-06-08 16:48:02 +0800

    c

--- app/routers/user.py
+++ app/routers/user.py
@@ -5,37 +5,32 @@ from ..dependencies import get_db
 
 router = APIRouter()
 
-@router.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
+@app.post("/companies/", response_model=schemas.Company)
+def create_company(company: schemas.CompanyCreate, db: Session = Depends(get_db)):
+    return crud.create_company(db=db, company=company)
 
-@router.put("/users/{user_id}/sensitive", response_model=schemas.User)
-def update_user_sensitive(user_id: int, user_update: schemas.UserUpdateSensitive, db: Session = Depends(get_db)):
-    db_user = crud.update_user_sensitive(db=db, user_id=user_id, user_update=user_update)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
+@app.get("/companies/", response_model=List[schemas.Company])
+def read_companies(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    companies = crud.get_companies(db, skip=skip, limit=limit)
+    return companies
 
-@router.put("/users/{user_id}/nonsensitive", response_model=schemas.User)
-def update_user_nonsensitive(user_id: int, user_update: schemas.UserUpdateNonSensitive, db: Session = Depends(get_db)):
-    db_user = crud.update_user_nonsensitive(db=db, user_id=user_id, user_update=user_update)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
+@app.get("/companies/{company_id}", response_model=schemas.Company)
+def read_company(company_id: int, db: Session = Depends(get_db)):
+    db_company = crud.get_company(db, company_id=company_id)
+    if db_company is None:
+        raise HTTPException(status_code=404, detail="Company not found")
+    return db_company
 
-@router.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(dependencies.get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
+@app.put("/companies/{company_id}", response_model=schemas.Company)
+def update_company(company_id: int, company: schemas.CompanyUpdate, db: Session = Depends(get_db)):
+    db_company = crud.get_company(db, company_id=company_id)
+    if db_company is None:
+        raise HTTPException(status_code=404, detail="Company not found")
+    return crud.update_company(db=db, company_id=company_id, company_update=company)
 
-@router.delete("/users/{user_id}", response_model=schemas.User)
-def delete_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.delete_user(db=db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
+@app.delete("/companies/{company_id}", response_model=schemas.Company)
+def delete_company(company_id: int, db: Session = Depends(get_db)):
+    db_company = crud.get_company(db, company_id=company_id)
+    if db_company is None:
+        raise HTTPException(status_code=404, detail="Company not found")
+    return crud.delete_company(db=db, company_id=company_id)
