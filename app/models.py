a81f08ac4ebabb9fe7172078bc0a2f1659c96eb9
Waiee Zainol <69743222+waiee@users.noreply.github.com>
2024-06-08 10:33:39 +0800

    c

--- app/models.py
+++ app/models.py
@@ -7,13 +7,18 @@ DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
 class User(Base):
     __tablename__ = "users"
+
     id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
+    company_name = Column(String, nullable=True)
+    company_sector = Column(String, nullable=True)
+    location = Column(String, nullable=True)
+    established_year = Column(Integer, nullable=True)
+    description = Column(String, nullable=True)
 
 Base.metadata.create_all(engine)
 
