17ebf089e585d028a26388b0324245b8ac3e187d
Waiee Zainol <69743222+waiee@users.noreply.github.com>
2024-06-08 10:01:00 +0800

    c

--- app/crud.py
+++ app/crud.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from app.models import User
-from app.schemas import UserCreate
+from app.schemas import UserCreate, UserUpdate
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -17,4 +17,26 @@ def create_user(db: Session, user: UserCreate):
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
+    return db_user
+
+def update_user(db: Session, user_id: int, user: UserUpdate):
+    db_user = get_user(db, user_id)
+    if db_user is None:
+        return None
+    if user.email:
+        db_user.email = user.email
+    if user.password:
+        db_user.hashed_password = pwd_context.hash(user.password)
+    if user.is_active is not None:
+        db_user.is_active = user.is_active
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def delete_user(db: Session, user_id: int):
+    db_user = get_user(db, user_id)
+    if db_user is None:
+        return None
+    db.delete(db_user)
+    db.commit()
     return db_user
